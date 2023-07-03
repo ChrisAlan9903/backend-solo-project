571655b02769257d41305ae5bdc195e099f607ee
ChrisAlan9903 <118709622+ChrisAlan9903@users.noreply.github.com>
2023-07-03 11:47:43 +0800

    Added gitignore and env files

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express()
+const morgan = require("morgan")
+
+
+// Load environment variables. See .env file for available variables.
+// This should be done before loading variables from process.env
+require("dotenv").config({
+    path:`.env.${process.env.NODE_ENV}`,
+})
 
 const PORT = 3000
 
