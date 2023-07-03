10fca216f1de82b58c8c39337a25703ec48bb783
ChrisAlan9903 <118709622+ChrisAlan9903@users.noreply.github.com>
2023-07-03 12:05:41 +0800

    Updated index.js files and env files

--- index.js
+++ index.js
@@ -9,8 +9,49 @@ require("dotenv").config({
     path:`.env.${process.env.NODE_ENV}`,
 })
 
-const PORT = 3000
+/* 
+Morgan configuration that logs the following:
+- the request body
+- the request params
+- the request query
+- the time of the request
+- the user agent
+*/
 
-app.listen( PORT, ()=>{
-    console.log( `🏎️ Server is running on PORT${PORT}`)
+const morganConfig = morgan(function (token, req, res){
+    return [
+        JSON.stringify(req.body),
+        JSON.stringify(req.params),
+        JSON.stringify(req.query),
+        token.date(req, res, "iso"),
+        req.headers["user-agent"],
+    ].join(" ");
+});
+
+//Middlewares
+app.use(express.json());
+app.use(morganConfig)
+
+const sequelize = require('./config/db.config')
+
+// Define routes here
+
+
+
+
+
+
+
+//Start the server
+
+const port = process.env.PORT || 3000;
+app.listen( PORT,async ()=>{
+
+    try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully.0")
+        console.log( `🏎️ Server is running on PORT${port}`)
+    } catch (error) {
+        econsole.log("Unable to connect to database:", error);
+    }
 })
