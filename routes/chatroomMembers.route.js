4beb7df12cb5d289a2f610782c45b16242811bcb
ChrisAlan9903 <118709622+ChrisAlan9903@users.noreply.github.com>
2023-07-04 02:50:24 +0800

    Add chatroomMem route

--- routes/chatroomMembers.route.js
+++ routes/chatroomMembers.route.js
@@ -5,3 +5,10 @@ const chatroomMembersController = require('../controllers/chatroomMembers.contro
 // TODO: Add middleware declaration here
 
 // add route endpoint here
+router.get('/chatroomMember', chatroomMembersController.getAllChatroomMember)
+router.get('/chatroomMember/:chatroomMemberId', chatroomMembersController.getChatroomMemberById)
+router.post('/chatroomMember', chatroomMembersController.createChatroomMember)
+router.put('/chatroomMember/:chatroomMemberId', chatroomMembersController.updateChatroomMember)
+router.delete('/chatroomMember/:chatroomMemberId', chatroomMembersController.deleteChatroomMember)
+
+module.exports = router
