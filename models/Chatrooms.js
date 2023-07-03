1d4a445a1327b36aba0eedf2613a6c9a00bda02b
ChrisAlan9903 <118709622+ChrisAlan9903@users.noreply.github.com>
2023-07-03 13:12:07 +0800

    Added Chatroom models

---
+++ models/Chatrooms.js
@@ -0,0 +1,59 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db.config");
+
+const Chatrooms = sequelize.define(
+  "Chatrooms",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      field: "id",
+    },
+    chatroomsTitle:{
+        type: DataTypes.STRING,
+        allowNull:false,
+        field:'chatroom_title'
+    },
+    circleId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "circles",
+        key: "id",
+      },
+      field: "circle_id",
+    },
+    creatorId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "chatroom_members",
+          key: "id",
+        },
+        field: "creator_id",
+      },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      field: "created_at",
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      field: "updated_at",
+    },
+  },
+  {
+    indexes: [
+      {
+        unique: true,
+        fields: ["chatroom_title", "circle_id)"],
+      },
+    ],
+    tableName: "chatrooms",
+    timestamps: false,
+  }
+);
+
+module.exports = Chatrooms;
