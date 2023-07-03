5ee4c41819c885a7664099823982fbef5f57ad2b
ChrisAlan9903 <118709622+ChrisAlan9903@users.noreply.github.com>
2023-07-04 02:34:12 +0800

    Add userRel route

--- routes/usersRelationship.route.js
+++ routes/usersRelationship.route.js
@@ -5,3 +5,11 @@ const userRelController = require('../controllers/usersRelationship.controller')
 // TODO: Add middleware declaration here
 
 // add route endpoint here
+
+router.get('/', userRelController.getAllUserRel)
+router.get('/:userRelId', userRelController.getUserRelById)
+router.post('/', userRelController.createUserRel)
+router.put('/:userRelId', userRelController.updateUserRel)
+router.delete('/:userRelId', userRelController.deleteUserRel)
+
+module.exports = router
