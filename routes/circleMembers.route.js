b3972a14086fe861181e7ba587ee9ec1b6a1cf80
ChrisAlan9903 <118709622+ChrisAlan9903@users.noreply.github.com>
2023-07-04 02:41:00 +0800

    add circleMem route

--- routes/circleMembers.route.js
+++ routes/circleMembers.route.js
@@ -5,3 +5,10 @@ const circleMembersController = require('../controllers/circleMembers.controller
 // TODO: Add middleware declaration here
 
 // add route endpoint here
+router.get('/circleMember', circleMembersController.getAllCircleMembers)
+router.post('/circleMember', circleMembersController.createCircleMembers)
+router.put('/circleMember/:circleMemberId', circleMembersController.updateCircleMembers)
+router.delete('/circleMember/:circleMemberId', circleMembersController.deleteCircleMembers)
+
+
+module.exports = router
