0e826e6e34391c7a358a726bb44164dbe2956977
ChrisAlan9903 <118709622+ChrisAlan9903@users.noreply.github.com>
2023-07-04 02:28:33 +0800

    Add users route

--- routes/users.route.js
+++ routes/users.route.js
@@ -5,3 +5,16 @@ const usersController = require('../controllers/users.controller')
 // TODO: Add middleware declaration here
 
 // add route endpoint here
+
+router.get('/', usersController.getAllUsers)
+
+router.get('/:userId', usersController.getUserById)
+
+router.post('/', usersController.createUser)
+
+router.put('/:userId', usersController.updateUser)
+
+router.delete('/:userId', usersController.deleteUser)
+
+
+module.exports = router
