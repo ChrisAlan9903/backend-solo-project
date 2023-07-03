cf13d6b70158981817becee54ebc0771daf56769
ChrisAlan9903 <118709622+ChrisAlan9903@users.noreply.github.com>
2023-07-04 02:44:09 +0800

    Add chatroom route

--- routes/chatrooms.route.js
+++ routes/chatrooms.route.js
@@ -5,3 +5,11 @@ const chatroomsController = require('../controllers/chatrooms.controller')
 // TODO: Add middleware declaration here
 
 // add route endpoint here
+router.get('/chatrooms', chatroomsController.getAllChatrooms)
+router.get('/chatrooms/:chatroomId', chatroomsController.getChatroomById)
+router.post('/chatrooms', chatroomsController.createChatrooms)
+router.put('/chatrooms/:chatroomId', chatroomsController.updateChatrooms)
+router.delete('/chatrooms/:chatroomId', chatroomsController.deleteChatrooms)
+
+
+module.exports = router
