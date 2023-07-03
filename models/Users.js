2ae3e04a35d05e5392f8f9b44635c7686b62c6b8
ChrisAlan9903 <118709622+ChrisAlan9903@users.noreply.github.com>
2023-07-03 12:25:15 +0800

    Created model files and added User model

---
+++ models/Users.js
@@ -0,0 +1,54 @@
+const {DataTypes} = require('sequelize')
+const sequelize = require('../config/db.config')
+
+const Users = sequelize.define(
+    "Users", {
+        id:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            field: "id"
+        },
+        username:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            field:"username"
+        },
+        email:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique:true,
+            field:"email"
+        },
+        password:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            field:"password"
+        },
+        dmAccess:{
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+            field:"dm_access"
+        },
+        createdAt:{
+            type:DataTypes.DATE,
+            allowNull:true,
+            field: "created_at"
+        },
+        updatedAt:{
+            type:DataTypes.DATE,
+            allowNull:true,
+            field: "updated_at"
+        },
+
+
+    },
+    {
+        tableName:"users",
+        timestamps: false
+    }
+
+)
+
+module.exports = Users
