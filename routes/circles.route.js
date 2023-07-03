196d10bf32365977d2188090127d3ea805908d90
ChrisAlan9903 <118709622+ChrisAlan9903@users.noreply.github.com>
2023-07-04 02:37:48 +0800

    Add circle route

--- routes/circles.route.js
+++ routes/circles.route.js
@@ -5,3 +5,10 @@ const circlesController = require('../controllers/circles.controller')
 // TODO: Add middleware declaration here
 
 // add route endpoint here
+
+router.get('/', circlesController.getAllCircle)
+router.post('/', circlesController.createCircle)
+router.put('/:circleId', circlesController.updateCircle)
+router.delete('/:circleId', circlesController.deleteCircle)
+
+module.exports = router
