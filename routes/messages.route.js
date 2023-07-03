ae927d72b86d8a00cdca9dd64746b86490fa663e
ChrisAlan9903 <118709622+ChrisAlan9903@users.noreply.github.com>
2023-07-04 02:52:45 +0800

    Add messages route

--- routes/messages.route.js
+++ routes/messages.route.js
@@ -5,3 +5,10 @@ const messagesController = require('../controllers/messages.controller')
 // TODO: Add middleware declaration here
 
 // add route endpoint here
+router.get('/', messagesController.getAllMessages)
+router.get('/:messageId', messagesController.getMessageById)
+router.post('/', messagesController.createMessage)
+router.put('/:messageId', messagesController.updateMessage)
+router.delete('/:messageId', messagesController.deleteMessage)
+
+module.exports = router
