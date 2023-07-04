27f54fcd03bb79c9c5eb8f11d9f903d8276c3ca2
ChrisAlan9903 <118709622+ChrisAlan9903@users.noreply.github.com>
2023-07-05 04:05:40 +0800

    Update auth to allow pw for login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,94 +1,110 @@
 // User Registration and Authentication
 //  - Implement user registration ✅
 //  - Implement user login (with token) ✅
-//  - Implement email verification upon registration 
+//  - Implement email verification upon registration
 
-const jwt = require("jsonwebtoken")
-const User = require('../models/Users')
-const { hashPassword, comparePassword } = require('../utils/bcrypt.util')
-
+const jwt = require("jsonwebtoken");
+const User = require("../models/Users");
+const { hashPassword, comparePassword } = require("../utils/bcrypt.util");
 
 // user registration function
-async function register(req, res){
-try {
+async function register(req, res) {
+  try {
     // Layer to check if user with same email already exist
     const isUserExist = await User.findOne({
-        where:{
-            email: req.body.email
-        }
-    })
+      where: {
+        email: req.body.email,
+      },
+    });
 
-    if(isUserExist){
-        throw `User already exists !`
+    if (isUserExist) {
+      throw `User already exists !`;
     }
 
     // Here to create user using data from request body. Request body must contain all required fields defined in User model.
 
-    const hashedPassword = hashPassword(req.body.password)
+    const hashedPassword = hashPassword(req.body.password);
     const newUser = await User.create({
-        ...req.body,
-        password: hashedPassword,
-    })
+      ...req.body,
+      password: hashedPassword,
+    });
 
     // Here to create registration verification token using email data(since it's unique)
     const token = jwt.sign(
-    {
+      {
         email: newUser.email,
-    },
-    process.env.SECRET_KEY
-    )
+      },
+      process.env.SECRET_KEY
+    );
     console.log(`token:`, token);
 
-    res.json({message:`Your account is successfully created`, newUser: newUser})
-
-} catch (error) {
-    res.status(500).json({ error: error }); 
-}
+    res.json({
+      message: `Your account is successfully created`,
+      newUser: newUser,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 }
 
 // user login function
-async function login(req, res){
-    try {
-        // Getting user input
-        const { email, password } = req.body
+async function login(req, res) {
+  try {
+    // Getting user input
+    const { email, password } = req.body;
 
-        // validate user input
-        if (!email && !password){
-            throw `Please fill both email and password`
-        }
-
-        const loginUser = await User.findOne({
-            where:{
-                email:email
-            }
-        })
-
-        if(!loginUser){
-            throw `User does not exists. Let's go and sign up !`
-        }
-
-        // validate user password
-        const checkPassword = comparePassword(password, loginUser.password)
-
-        if(!checkPassword) throw `Wrong password / Password does not match !`
-
-        //if both email and password match, generate login token using several user properties
-
-        const loginToken = jwt.sign(
-            {
-                id:loginUser.id, email: loginUser.email, role: loginUser.role
-            },
-            process.env.SECRET_KEY
-            )
-    res.json({message:`You're in !`, accessToken: loginToken})
-
-    } catch (error) {
-    res.status(500).json({ error: error });
+    // validate user input
+    if (!email && !password) {
+      throw `Please fill both email and password`;
     }
-}
 
+    const loginUser = await User.findOne({
+      where: {
+        email: email,
+      },
+      // Include password field. By default, password field is excluded in every query. See User model.
+      attributes: { include: "password" },
+    });
+
+    console.log(`checkpoint 1`);
+
+    if (!loginUser) {
+      console.log(`checkpoint 2`);
+
+      throw `User does not exists. Let's go and sign up !`;
+    }
+
+    console.log(`checkpoint 3`);
+    console.log(`login password:`, password);
+    console.log(`db user password:`, loginUser.password);
+
+    // validate user password
+    const checkPassword = comparePassword(password, loginUser.password);
+
+    console.log(`checkpoint 4`);
+
+    if (!checkPassword) throw `Wrong password / Password does not match !`;
+
+    //if both email and password match, generate login token using several user properties
+    console.log(`checkpoint 5`);
+
+    const loginToken = jwt.sign(
+      {
+        id: loginUser.id,
+        email: loginUser.email,
+        role: loginUser.role,
+      },
+      process.env.SECRET_KEY
+    );
+    console.log(`checkpoint 6`);
+
+    res.json({ message: `You're in !`, accessToken: loginToken });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+}
 
 module.exports = {
-    register,
-    login,
-}
+  register,
+  login,
+};
