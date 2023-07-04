5e1f047cd60af1d54fe00ce1efa25c60920c87b3
ChrisAlan9903 <118709622+ChrisAlan9903@users.noreply.github.com>
2023-07-05 07:58:55 +0800

    Add auth midware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,39 @@
+const jwt = require('jsonwebtoken')
+
+function verifyToken (req, res, next){
+    // Get auth header value
+    const token = req.headers["Authorization"]
+
+    try {
+        // Check if token is provided
+        if(!token) throw `No token provided !`
+
+        // Verify & decode token
+        const decodedToken = jwt.verify(token, process.env.SECRET_KEY)
+
+        // Set user in the request object
+        req.user = decodedToken
+    } catch (error) {
+        return res.status(401).json({error: error})
+        
+    }
+
+    // If all OK - proceed to next middleware (if any)
+    return next()
+}
+
+function checkRole(roles){
+    return (req, res, next) =>{
+
+        if (!roles.includes(req.user.role)){
+            return res.status(401).json({error: "Unauthorized"})
+        }
+
+        return next()
+    }
+}
+
+module.exports = {
+    verifyToken,
+    checkRole
+}
