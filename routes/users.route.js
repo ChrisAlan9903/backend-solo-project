dc62400672b57efd05dd4a506e4513522f827425
ChrisAlan9903 <118709622+ChrisAlan9903@users.noreply.github.com>
2023-07-05 08:04:36 +0800

    update user route: add autho

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,20 +1,21 @@
 const express = require('express')
 const router = express.Router()
 const usersController = require('../controllers/users.controller')
+const { verifyToken, checkRole } = require('../middlewares/auth.middleware')
 
 // TODO: Add middleware declaration here
 
 // add route endpoint here
 
-router.get('/', usersController.getAllUsers)
+router.get('/',verifyToken, checkRole(["admin","user"]), usersController.getAllUsers)
 
-router.get('/:userId', usersController.getUserById)
+router.get('/:userId',verifyToken, checkRole(["admin","user"]), usersController.getUserById)
 
-router.post('/', usersController.createUser)
+router.post('/',verifyToken, checkRole(["admin"]), usersController.createUser)
 
-router.put('/:userId', usersController.updateUser)
+router.put('/:userId',verifyToken, checkRole(["admin","user"]), usersController.updateUser)
 
-router.delete('/:userId', usersController.deleteUser)
+router.delete('/:userId',verifyToken, checkRole(["admin","user"]), usersController.deleteUser)
 
 
 module.exports = router
