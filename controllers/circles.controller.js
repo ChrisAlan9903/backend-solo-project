5b660b1d62c48caf2d2ffdab3e42e9d832fa70e6
ChrisAlan9903 <118709622+ChrisAlan9903@users.noreply.github.com>
2023-07-17 20:35:48 +0800

    added autho to Circle (incomplete)

--- controllers/circles.controller.js
+++ controllers/circles.controller.js
@@ -6,85 +6,122 @@ async function getAllCircle(req, res) {
     // TODO: add authorization here--⚠️⚠️
 
     //⚠️⚠️to be updated
-  
 
     const circles = await Circle.findAll();
     console.log(`CP 2`);
     res.json(circles);
- 
   } catch (error) {
     res.status(500).json({ error: error });
   }
 }
-async function getCirclesForUser(req,res){
+async function getCirclesForUser(req, res) {
   try {
-    //add authorization here
+    //TODO: add authorization here
+    //Permission: User can only get Circles that they are involved
 
-   const creatorId = req.params.creatorId
-   console.log(`creatorId:`, creatorId)
+    const creatorId = req.params.creatorId;
+    console.log(`creatorId:`, creatorId);
 
-   const userCircles = await Circle.findAll({
-    where:{
-      creatorId: creatorId
-    }
-   })
-   res.json(userCircles)
-
-    
+    const userCircles = await Circle.findAll({
+      where: {
+        creatorId: creatorId,
+      },
+    });
+    res.json(userCircles);
   } catch (error) {
     res.status(500).json({ error: error });
-    
   }
-} 
+}
 
 async function createCircle(req, res) {
   try {
+    console.log(`req user id:`, req.user.id);
+
     const circle = await Circle.create({
-        ...req.body});
+      ...req.body,
+      creatorId: req.user.id,
+    });
     res.json(circle);
   } catch (error) {
     res.status(500).json({ error: error });
   }
 }
+// Note for createCircle: can add extra feature where admin can create circle for other people as well. For now the creatorId for a circle is based on solely on the user's id that created the circle.
 
 
 async function updateCircle(req, res) {
-  // TODO: add authorization here--⚠️⚠️
- 
-    try {
-      const updatedCircle = await Circle.update(req.body, {
+  try {
+    // TODO: add authorization here--⚠️⚠️
+    // Permission: User can only update their own Circle
+
+    const userId = req.user.id;
+    const circleId = parseInt(req.params.circleId);
+
+    console.log(circleId);
+
+    const getCircle = await Circle.findByPk(circleId);
+    const { creatorId } = getCircle;
+
+    console.log(`gottencircle:`, getCircle);
+    console.log(` destructured creatorId:`, creatorId);
+
+    if (req.user.role !== "admin") {
+      if (userId !== creatorId)
+        throw `Circle update DENIED: Not your Circle b*tch`;
+    }
+
+    const updatedCircle = await Circle.update(
+      { ...req.body, creatorId: creatorId },
+      {
         where: {
           id: parseInt(req.params.circleId),
         },
-      });
-      res.json(updatedCircle)
-    } catch (error) {
-      res.status(500).json({ error: error });
-    }
+      }
+    );
+    res.json(updatedCircle);
+  } catch (error) {
+    res.status(500).json({ error: error });
   }
-
+}
 
 async function deleteCircle(req, res) {
-    try {
-  // TODO: add authorization here--⚠️⚠️
-        
-        const deletedCircle = await Circle.destroy({
-            where:{
-                id: parseInt(req.params.circleId)
-            }
-        })
-        res.json(deletedCircle)
+  try {
+    // TODO: add authorization here--⚠️⚠️
+    // Permission: User can only update their own Circle
 
-    } catch (error) {
-      res.status(500).json({ error: error });
-        
+    const userId = req.user.id;
+    const circleId = parseInt(req.params.circleId);
+
+    console.log(circleId);
+    
+    const getCircle = await Circle.findByPk(circleId);
+    console.log(`CP: getCircle =`, getCircle? 'exist':'not exist');
+   
+    if(!getCircle) throw `Circle does not Exist`
+
+    const { creatorId } = getCircle;
+
+    console.log(` destructured creatorId:`, creatorId);
+
+    if (req.user.role !== "admin") {
+      if (userId !== creatorId)
+        throw `Circle delete DENIED: Not your Circle b*tch`;
     }
+    const deletedCircle = await Circle.destroy({
+      where: {
+        id: parseInt(req.params.circleId),
+      },
+    });
+    res.json(deletedCircle);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 }
 
 module.exports = {
-    getAllCircle,
-    getCirclesForUser,
-    createCircle,
-    updateCircle,
-    deleteCircle,
-}
+  getAllCircle,
+  getCirclesForUser,
+  createCircle,
+  updateCircle,
+  deleteCircle,
+};
