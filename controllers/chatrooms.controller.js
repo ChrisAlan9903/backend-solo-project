69cc77c8fa42bef8340695733178ff10bb071921
ChrisAlan9903 <118709622+ChrisAlan9903@users.noreply.github.com>
2023-07-27 20:06:35 +0800

    just change some typo

--- controllers/chatrooms.controller.js
+++ controllers/chatrooms.controller.js
@@ -1,85 +1,78 @@
-const Chatrooms = require('../models/Chatrooms')
+const Chatrooms = require("../models/Chatrooms");
 
 // GET request here
-async function getAllChatrooms(req,res){
-    try {
+async function getAllChatrooms(req, res) {
+  try {
     // TODO: add authorization here--⚠️⚠️
 
-        const chatrooms = await Chatrooms.findAll()
-        res.json(chatrooms)
-        
-    } catch (error) {
-        res.status(500).json({ error: error });  
-
-}
+    const chatrooms = await Chatrooms.findAll();
+    res.json(chatrooms);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 }
 
-async function getChatroomById(req,res){
-    try {
+async function getChatroomById(req, res) {
+  try {
     // TODO: add authorization here--⚠️⚠️
-        
-    const chatroom = await Chatrooms.findByPk(parseInt(req.params.chatroomId))
-    res.json(chatroom)
-        
-    } catch (error) {
-        res.status(500).json({ error: error });  
 
-}
+    const chatroom = await Chatrooms.findByPk(parseInt(req.params.chatroomId));
+    res.json(chatroom);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 }
 
 // CREATE request here
-async function createChatrooms(req,res){
-    try {
+async function createChatrooms(req, res) {
+  try {
     // TODO: add authorization here--⚠️⚠️
-       
-    const chatroom = await Chatrooms.create({
-        ...req.body
-    })
-    res.json(chatroom)
-    } catch (error) {
-        res.status(500).json({ error: error });  
 
-}
+    const chatroom = await Chatrooms.create({
+      ...req.body,
+    });
+    res.json(chatroom);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 }
 
 // UPDATE request here
-async function updateChatrooms(req,res){
-    try {
+async function updateChatrooms(req, res) {
+  try {
     // TODO: add authorization here--⚠️⚠️
-      
-    const updatedChatroom = await Chatrooms.update(req.body, {
-        where: {
-            id: parseInt(req.params.chatroomId)
-        }
-    })
-    res.json(updatedChatroom)
-    } catch (error) {
-        res.status(500).json({ error: error });  
 
-}
+    const updatedChatroom = await Chatrooms.update(req.body, {
+      where: {
+        id: parseInt(req.params.chatroomId),
+      },
+    });
+    res.json(updatedChatroom);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 }
 
 // DELETE request here
-async function deleteChatrooms(req,res){
-    try {
+async function deleteChatrooms(req, res) {
+  try {
     // TODO: add authorization here--⚠️⚠️
-       
-    const deletedChatroom = await Chatrooms.destroy({
-        where:{
-            id: parseInt(req.params.chatroomId)
-        }
-    })
-    res.json(deletedChatroom)
-    } catch (error) {
-        res.status(500).json({ error: error });  
 
-}
+    const deletedChatroom = await Chatrooms.destroy({
+      where: {
+        id: parseInt(req.params.chatroomId),
+      },
+    });
+    res.json(deletedChatroom);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 }
 
 module.exports = {
-    getAllChatrooms,
-    getChatroomById,
-    createChatrooms,
-    updateChatrooms,
-    deleteChatrooms,
-}
+  getAllChatrooms,
+  getChatroomById,
+  createChatrooms,
+  updateChatrooms,
+  deleteChatrooms,
+};
