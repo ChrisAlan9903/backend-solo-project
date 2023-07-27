28d71813d34e2ee0aa9b4b0673a21fa485f08d2d
ChrisAlan9903 <118709622+ChrisAlan9903@users.noreply.github.com>
2023-07-27 14:11:13 +0800

    updated index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const app = express()
-const morgan = require("morgan")
-
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
 
 // Load environment variables. See .env file for available variables.
 // This should be done before loading variables from process.env
 require("dotenv").config({
-    path:`.env.${process.env.NODE_ENV}`,
-})
+  path: `.env.${process.env.NODE_ENV}`,
+});
 
 /* 
 Morgan configuration that logs the following:
@@ -18,58 +17,56 @@ Morgan configuration that logs the following:
 - the user agent
 */
 
-const morganConfig = morgan(function (token, req, res){
-    return [
-        JSON.stringify(req.body),
-        JSON.stringify(req.params),
-        JSON.stringify(req.query),
-        token.date(req, res, "iso"),
-        req.headers["user-agent"],
-    ].join(" ");
+const morganConfig = morgan(function (token, req, res) {
+  return [
+    JSON.stringify(req.body),
+    JSON.stringify(req.params),
+    JSON.stringify(req.query),
+    token.date(req, res, "iso"),
+    req.headers["user-agent"],
+  ].join(" ");
 });
 
 //Middleware
 app.use(express.json());
-app.use(morganConfig)
+app.use(morganConfig);
 
-const sequelize = require('./config/db.config')
+const sequelize = require("./config/db.config");
 
 // Define routes here
-const authRoute = require('./routes/auth.route')
-const usersRoute = require('./routes/users.route')
-const circlesRoute = require('./routes/circles.route')
-const circleMemberRoute = require('./routes/circleMembers.route')
-const messagesRoute = require('./routes/messages.route')
-// const usersRelRoute = require('./routes/usersRelationship.route')
-// const chatroomsRoute = require('./routes/chatrooms.route')
+const authRoute = require("./routes/auth.route");
+const usersRoute = require("./routes/users.route");
+const foodItemsRoute = require("./routes/foodItems.route");
+const ordersRoute = require("./routes/orders.route");
+const orderItemsRoute = require("./routes/orderItems.route");
+const categoriesRoute = require("./routes/categories.route");
+const vendorsRoute = require("./routes/vendors.route");
 // const chatroomMemberRoute = require('./routes/chatroomMembers.route')
 
-app.use('/auth', authRoute)
-app.use('/users', usersRoute)
-app.use('/circles', circlesRoute)
-app.use('/circleMembers', circleMemberRoute)
-app.use('/messages', messagesRoute)
-// app.use('/usersRelationship', usersRelRoute)
-// app.use('/circles', chatroomsRoute)
+app.use("/auth", authRoute);
+app.use("/users", usersRoute);
+app.use("/foodItems", foodItemsRoute);
+app.use("/orders", ordersRoute);
+app.use("/orderItems", orderItemsRoute);
+app.use("/categories", categoriesRoute);
+app.use("/vendors", vendorsRoute);
 // app.use('/circles', chatroomMemberRoute)
 
 // Health check
-app.get('/', (req,res)=>{
-    res.status(200).send({message: `Health OK : ${ process.env.NODE_ENV}`})
-    // res.send(`Health OK : `, process.env.NODE_ENV)
-})
-
+app.get("/", (req, res) => {
+  res.status(200).send({ message: `Health OK : ${process.env.NODE_ENV}` });
+  // res.send(`Health OK : `, process.env.NODE_ENV)
+});
 
 //Start the server
 
 const port = process.env.PORT || 3000;
-app.listen( port,async ()=>{
-
-    try {
-        await sequelize.authenticate();
-        console.log("Connection has been established successfully.0")
-        console.log( `🏎️ Server is running on PORT ${port}`)
-    } catch (error) {
-        console.log("Unable to connect to database:", error);
-    }
-})
+app.listen(port, async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.0");
+    console.log(`🏎️ Server is running on PORT ${port}`);
+  } catch (error) {
+    console.log("Unable to connect to database:", error);
+  }
+});
